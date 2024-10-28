3931482789e2cd25672902d15d94ef361a73860d
10terabyte <marclyndond@gmail.com>
2024-10-28 20:00:11 +0100

    feat: set up initial routing and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
+import { StreamingComponent } from './streaming/streaming.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'upload', component: UploadComponent},
+    {path: 'streaming/:id', component: StreamingComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
