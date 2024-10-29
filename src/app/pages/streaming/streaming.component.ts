166c4685c1d9bd9964b5fb37610f3451a8328cfb
10terabyte <marclyndond@gmail.com>
2024-10-29 11:00:18 +0100

    feat: implement video streaming functionality

--- src/app/pages/streaming/streaming.component.ts
+++ src/app/pages/streaming/streaming.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environment/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-streaming',
@@ -8,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './streaming.component.scss'
 })
 export class StreamingComponent {
+  backendUrl = environment.BACKEND_BASE_URL;
+  videoSource: string = ''; 
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    
+    this.route.paramMap.subscribe(params => {
+      const videoId = params.get('id');
+      if (videoId) {
+        this.videoSource = `${this.backendUrl}/api/videos/stream/${videoId}`;
+        console.log(this.videoSource)
+      }
+    });
+  }
 }
